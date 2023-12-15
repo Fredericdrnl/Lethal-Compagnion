3557f9418890f8f7b003091417e5dfafea0b6f23
Tom VALLART <tomvallart62@gmail.com>
2023-12-15 11:25:44 +0100

    Add command structure

--- src/bot/commands/Ping.py
+++ src/bot/commands/Ping.py
@@ -13,11 +13,11 @@ class PingCommand(commands.Cog):
         """Test pour ping le bot pour voir les MS.
         """
         embedPing = discord.Embed(title=f'{round(self.bot.latency * 1000)}ms',
-                                    description="Ping of PUGS bot.",
+                                    description="Ping of LethalCompagnion bot.",
                                     colour=discord.Colour.from_rgb(240, 128, 128))
         
-        embedPing.set_footer(text="By WarFlay#8465",
-                            icon_url="https://i.goopics.net/encbhm.png")
+        # embedPing.set_footer(text="By nous",
+        #                     icon_url="https://i.goopics.net/encbhm.png")
         
 
         await ctx.send(embed=embedPing)
