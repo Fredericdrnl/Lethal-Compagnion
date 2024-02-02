239cabfa14e6cdf5ba2e2744b9a0effb786beecd
Tabooret <ben.fouurnier@gmail.com>
2024-02-02 13:38:20 +0100

    add(database): add database to project

---
+++ data/db/setup.sql
@@ -0,0 +1,30 @@
+CREATE TABLE Monsters(
+    id_monster SERIAL PRIMARY KEY,
+    name_monster VARCHAR(50),
+    entry_monster VARCHAR(500),
+    particularity VARCHAR(500),
+    strategy VARCHAR(500)
+)
+
+CREATE TABLE Scrap(
+    id_scrap SERIAL PRIMARY KEY,
+    name_scrap VARCHAR(500),
+    entry_scrap VARCHAR(500),
+    cost_scrap INTEGER,
+    weight INTEGER,
+)
+
+CREATE TABLE Moons(
+    id_moon SERIAL PRIMARY KEY,
+    name_moon VARCHAR(500),
+    difficulty VARCHAR(500),
+    cost_moon INTEGER
+)
+
+CREATE TABLE Store(
+    id_store SERIAL PRIMARY KEY,
+    name_store VARCHAR(500),
+    entry_store VARCHAR(500),
+    cost_store INTEGER,
+    weight INTEGER,
+)
