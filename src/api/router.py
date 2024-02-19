a894f3738f9b497ccfbee3511061dc069a71cbb0
Frédéric DOURNEL <fredericdournel@gmail.com>
2024-02-19 14:17:29 +0100

    edit(router.py) :  add routes for commands

--- src/api/router.py
+++ src/api/router.py
@@ -1,5 +1,5 @@
 import flask
-from data import getMonster
+from data import getMonster, getMonsters, getItems, getMoons, getStoreItems
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,13 +8,27 @@ app.config['DEBUG'] = True
 def acceuil():
     return "API du bot discord Lethal Compagny"
 
-@app.route('/Monsters/<str:MonsterName>', methods=['GET'])
-def Monsters(MonsterName : str):
+@app.route('/Monsters/test', methods=['GET'])
+def Bestiary(MonsterName : str):
     return getMonster(MonsterName)
 
-# @app.route('/Monsters/test', methods=['GET'])
-# def Monsters():
-#     return getMonster()
+@app.route('/Monsters/', methods=['GET'])
+def Monsters():
+    return getMonsters()
+
+@app.route('/Moons/', methods=['GET'])
+def Moons():
+    return getMoons()
+
+@app.route('/Items/', methods=['GET'])
+def Items():
+    return getItems()
+
+@app.route('/StoreItems/', methods=['GET'])
+def StoreItems():
+    return getStoreItems()
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
