229eaff3163a10e196737e32381fd05d78458416
Frédéric DOURNEL <fredericdournel@gmail.com>
2024-02-20 17:05:15 +0100

    edit(router.py) add routes for news commands

--- src/api/router.py
+++ src/api/router.py
@@ -1,34 +1,48 @@
+from data import *
 import flask
-from data import getMonster, getMonsters, getItems, getMoons, getStoreItems
 
 app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET'])
-def acceuil():
-    return "API du bot discord Lethal Compagny"
+def accueil():
+    return "API du bot discord Lethal Company"
 
-@app.route('/Monsters/test', methods=['GET'])
-def Bestiary(MonsterName : str):
+# ■■■■■■■■■■■■■■■■■■■■■■■ Monster ■■■■■■■■■■■■■■■■■■■■■■■■■■■■ #
+@app.route('/Monsters/<MonsterName>', methods=['GET'])
+def Monster(MonsterName : str):
     return getMonster(MonsterName)
 
 @app.route('/Monsters/', methods=['GET'])
 def Monsters():
     return getMonsters()
 
+# ■■■■■■■■■■■■■■■■■■■■■■■ Moon ■■■■■■■■■■■■■■■■■■■■■■■■■■■■ #
+@app.route('/Moons/<MoonName>', methods=['GET'])
+def Moon(MoonName : str):
+    return getMoon(MoonName)
+
 @app.route('/Moons/', methods=['GET'])
 def Moons():
     return getMoons()
+
+# ■■■■■■■■■■■■■■■■■■■■■■■ Item ■■■■■■■■■■■■■■■■■■■■■■■■■■■■ #
+@app.route('/Items/<ItemName>', methods=['GET'])
+def Item(ItemName : str):
+    return getItem(ItemName)
 
 @app.route('/Items/', methods=['GET'])
 def Items():
     return getItems()
 
+# ■■■■■■■■■■■■■■■■■■■■■■■ Store Item ■■■■■■■■■■■■■■■■■■■■■■■■■■■■ #
+@app.route('/StoreItems/<StoreItemName>', methods=['GET'])
+def StoreItem(StoreItemName):
+    return getStoreItem(StoreItemName)
+
 @app.route('/StoreItems/', methods=['GET'])
 def StoreItems():
     return getStoreItems()
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
