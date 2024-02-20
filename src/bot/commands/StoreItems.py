10c580ba4e12a321ffc0accb11a7efbd5e4d7b79
Frédéric DOURNEL <fredericdournel@gmail.com>
2024-02-20 17:04:15 +0100

    edit(storeitem) : storeitem and storeitems commands launch

--- src/bot/commands/StoreItems.py
+++ src/bot/commands/StoreItems.py
@@ -11,7 +11,7 @@ class StoreItemsCommand(commands.Cog):
 
     @commands.command()
     async def storeItems(self, ctx):
-        """Show information of a monster."""
+        """Show information of all StoreItems"""
         response = requests.get("http://127.0.0.1:5000/StoreItems/")
         # Vérifier si la requête a réussi (code de statut HTTP 200)
         if response.status_code == 200:
